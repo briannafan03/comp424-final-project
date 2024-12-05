e0fb1395ef31a7fef9a3d1f9dfd8b44b0be2e665
Brianna Fan <143474047+briannafan03@users.noreply.github.com>
2024-12-04 21:02:03 -0500

    Update __init__.py
    revert to default

--- agents/__init__.py
+++ agents/__init__.py
@@ -2,5 +2,3 @@ from .random_agent import RandomAgent
 from .human_agent import HumanAgent
 from .student_agent import StudentAgent
 from .gpt_greedy_corners_agent import StudentAgent
-
-from .second_agent import SecondAgent
